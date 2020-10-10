9095dccbaecf5656a0c22da9059001b7059d83c7
Alvaro Lacouture <lacouture100@gmail.com>
2020-10-10 15:14:18 -0400

    Added PORT through dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const moment = require("moment"); //include the moment package
 const sunCalc = require("suncalc");
@@ -17,28 +17,24 @@ let logTime = moment().format().toString();
 // Get suncalc times
 var times = sunCalc.getTimes(new Date(), lat, long);
 
-var sunrise                 = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
-var sunriseEnd              = times.sunriseEnd.getHours() + ':' + times.sunriseEnd.getMinutes();
-var morningGoldenHourEnd    = times.goldenHourEnd.getHours() + ':' + times.goldenHourEnd.getMinutes();
-var solarNoon               = times.solarNoon.getHours() + ':' + times.solarNoon.getMinutes();
-var goldenHour              = times.goldenHour.getHours() + ':' + times.goldenHour.getMinutes();
-var sunsetStart             = times.goldenHour.getHours() + ':' + times.goldenHour.getMinutes();
-var sunset                  = times.sunset.getHours() + ':' + times.sunset.getMinutes();
-var dusk                    = times.dusk.getHours() + ':' + times.dusk.getMinutes();
-var night                   = times.night.getHours() + ':' + times.night.getMinutes();
-var nadir                   = times.nadir.getHours() + ':' + times.nadir.getMinutes();
-var nightEnd                = times.nightEnd.getHours() + ':' + times.nightEnd.getMinutes();
-var nauticalDawn            = times.nauticalDawn.getHours() + ':' + times.nauticalDawn.getMinutes();
-var dawn                    = times.dawn.getHours() + ':' + times.dawn.getMinutes();
+var sunrise = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
+var sunriseEnd = times.sunriseEnd.getHours() + ':' + times.sunriseEnd.getMinutes();
+var morningGoldenHourEnd = times.goldenHourEnd.getHours() + ':' + times.goldenHourEnd.getMinutes();
+var solarNoon = times.solarNoon.getHours() + ':' + times.solarNoon.getMinutes();
+var goldenHour = times.goldenHour.getHours() + ':' + times.goldenHour.getMinutes();
+var sunsetStart = times.goldenHour.getHours() + ':' + times.goldenHour.getMinutes();
+var sunset = times.sunset.getHours() + ':' + times.sunset.getMinutes();
+var dusk = times.dusk.getHours() + ':' + times.dusk.getMinutes();
+var night = times.night.getHours() + ':' + times.night.getMinutes();
+var nadir = times.nadir.getHours() + ':' + times.nadir.getMinutes();
+var nightEnd = times.nightEnd.getHours() + ':' + times.nightEnd.getMinutes();
+var nauticalDawn = times.nauticalDawn.getHours() + ':' + times.nauticalDawn.getMinutes();
+var dawn = times.dawn.getHours() + ':' + times.dawn.getMinutes();
 var sunPos = sunCalc.getPosition(new Date(), /*Number*/ lat, /*Number*/ long);
 
 console.log(sunPos)
 
 
-
-
-
-//absolute path to my index.html file. 
 //__dirname is the absolute path of the directory containing the currently executing file.
 const indexPath = path.join(__dirname, 'views/index.html');
 
@@ -53,17 +49,8 @@ app.use(express.json());
 //a '/' request will send you to the index.html file
 app.get('/', (req, res) => res.sendFile(indexPath))
 
-/*
-//Define our GET function.
-function getToppings() {
-    //grab the toppings json from the data folder on root
-    const toppingsJSON = fs.readFileSync(path.join(__dirname, '/data/toppings.json'));
-    //parse the toppings json
-    const toppings = JSON.parse(toppingsJSON);
-    return toppings
-}
-*/
-function getTimes(){
+
+function getTimes() {
 
     //Read our sunEvents in order to update the data on each request.
     const sunEventsJSON = fs.readFileSync(path.join(__dirname, '/data/suncalcData.json'));
@@ -88,42 +75,7 @@ function getTimes(){
     return sunEvents;
 }
 
-/*
-//Define our POST function.
-function addTopping(topping) {
-    //read from the toppings json before adding a topping
-    const toppings = getToppings();
-    //add the topping to the 'pizzaToppings' item in the json
-    toppings.pizzaToppings.push(topping);
-    //update the pizzaToppings file with the new toppings
-    fs.writeFileSync(path.join(__dirname, '/data/toppings.json'), JSON.stringify(toppings));
-    return toppings
-}
-
-function addTime(sunEvent){
-
-}
-
-//Define our DELETE function.
-function deleteTopping(deleteTopping) {
-    //read from the toppings json before deleting a topping
-    const toppings = getToppings();
-    //Only keep the toppings that are different from the deleteTopping variable
-    toppings.pizzaToppings = toppings.pizzaToppings.filter(topping => topping !== deleteTopping);
-    //update the pizzaToppings file with the new toppings
-    fs.writeFileSync(path.join(__dirname, '/data/toppings.json'), JSON.stringify(toppings));
-    return toppings
-}
-*/
-/*
 //Express GET request
-app.get("/toppings", (req, res) => {
-    //read from the toppings json
-    const toppings = getToppings();
-    // Updated list will be returned by API
-    res.json(
-        */
-        //Express GET request
 app.get("/sun", (req, res) => {
     //read from the toppings json
     const sunEvents = getTimes();
@@ -131,30 +83,7 @@ app.get("/sun", (req, res) => {
     // Updated list will be returned by API
     res.json(sunEvents);
 });
-/*
 
-//Express POST request
-app.post("/toppings", (req, res) => {
-    //grab the 'topping' from the POST request. The request must be in the {"topping":"whatever"} format.
-    const topping = req.body.topping;
-    //add the topping to the toppings list
-    const toppings = addTopping(topping);
-    // Updated list will be returned by API
-    res.json(toppings);
-});
-
-//Express DELETE request
-app.delete("/toppings/:name", (req, res) => {
-    //set the name at the end of the request as the name of the topping to delete
-    const toppingToDelete = req.params.name;
-    //update the toppings file after deleting the topping
-    const toppings = deleteTopping(toppingToDelete);
-    // Updated list will be returned by API
-    res.json(toppings);
-});
-
-*/
-
-//Open the port
-app.listen(port, () => console.log(`Server app listening on port ${port}!`))
+//Open the PORT
+app.listen(PORT, () => console.log(`Server app listening on PORT ${PORT}!`))
 getTimes()
